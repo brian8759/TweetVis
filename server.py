ed122e51132d85cb81878453bcf2d40e62faacce
brian8759 <brian8759@gmail.com>
2014-11-08 00:28:59 -0500

    add 'preprocessTweet()'

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 
 import zerorpc
 import json
+import re
 import nltk.classify.util
 from nltk.classify import NaiveBayesClassifier
 from nltk.corpus import movie_reviews
@@ -15,6 +16,22 @@ def extract_features(document):
 	for word in word_features:
 		features['contains(%s)' % word] = (word in document_words)
 	return features
+
+def preprocessTweet(tweet):
+	''' here tweet is tweet.text '''
+	# convert to lower case
+	tweet = tweet.lower()
+	# convert url to URL
+	tweet = re.sub('((www\.[\s]+)|(https?://[^\s]+))','URL',tweet)
+	# convert @username to AT_USER
+	tweet = re.sub('@[^\s]+','AT_USER',tweet)
+	# remove additional white spaces
+	tweet = re.sub('[\s]+', ' ', tweet)
+	# replace hashtag with regular word
+	tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
+	# trim
+	tweet = tweet.strip()
+	return tweet
 
 class HelloRPC(object):
 	def __init__(self):
@@ -30,9 +47,11 @@ class HelloRPC(object):
 
 	def analyze(self, tweet):
 		''' tweet is a json file, we need to extract the text part, and do feature extraction, then use classifier '''
+		''' before analyzing tweet, we need to preprocess it'''
 		tweetString = json.loads(tweet)
 		text = tweetString['text']
-		attitude = self.classifier.classify(extract_features(text.split()))
+		processedText = preprocessTweet(text)
+		attitude = self.classifier.classify(extract_features(processedText.split()))
 		
 s = zerorpc.Server(HelloRPC())
 s.bind("tcp://127.0.0.1:4242")
