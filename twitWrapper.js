ee59a67e80164642be8fb03450fa0c1fc300be38
brian8759 <brian8759@gmail.com>
2015-03-13 18:22:58 -0400

    adding key

--- twitWrapper.js
+++ twitWrapper.js
@@ -1,10 +1,10 @@
 var Twit = require('twit');
 
 var T = new Twit({
-    consumer_key:         '',
-    consumer_secret:      '',
-    access_token:         '',
-    access_token_secret:  ''
+    consumer_key:         'sBBsyWD41S7RxyZQYm5PkSv8l',
+    consumer_secret:      'jWx89GtOCUkfsbD6M83BJd4C2u2WoMNLL8yXuyPSkmooWAqZtf',
+    access_token:         '2859539890-Gx8ek68KiQA7237kKxP8G0sOlRTIVO2ojwh95Qf',
+    access_token_secret:  'oXVAAiWl7yiblDFY9vjperCT1tSTYR62SMjxsf8TI2uw3'
 });
 
 module.exports = T;
